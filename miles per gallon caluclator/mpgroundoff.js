d9a230f201bee23461fd06bbf4b51c1ecf03fdf6
Nanjesh Ramesh <nanjeshramesh7@gmail.com>
2019-10-10 10:50:01 -0700

    Add files via upload

---
+++ miles per gallon caluclator/mpgroundoff.js
@@ -0,0 +1,1 @@
+var milesDriven=window.prompt("Enter the miles driven:"),gallons=window.prompt("Enter the size of your tank in gallons:"),mpg=milesDriven/gallons;document.write("You drove: "+milesDriven+"<br>"),document.write("Your tank size: "+gallons+"<br>"),document.write("You got "+parseInt(mpg,10)+" miles per gallon on this trip.");
